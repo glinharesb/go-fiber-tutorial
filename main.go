41a6c651ca7c62f72eec73f664c63d303773727c
Gabriel Linhares <glinharesb@gmail.com>
2021-12-24 14:45:28 -0300

    tutorial

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/glinharesb/go-fiber-tutorial/book"
+	"github.com/glinharesb/go-fiber-tutorial/database"
+	"github.com/gofiber/fiber"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/book", book.GetBooks)
+	app.Get("/api/v1/book/:id", book.GetBook)
+	app.Post("/api/v1/book", book.NewBook)
+	app.Delete("/api/v1/book/:id", book.DeleteBook)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "books.db")
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("Dtabase conneciton successfully opened")
+
+	database.DBConn.AutoMigrate(&book.Book{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	defer database.DBConn.Close()
+
+	setupRoutes(app)
+
+	app.Listen(3000)
+}
